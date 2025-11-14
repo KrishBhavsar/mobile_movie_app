c8de1245ce3bb9d89fb4c1c63f28bddad158b1ec
KrishBhavsar <bhavsarkrish308@gmail.com>
2025-11-14 14:22:22 +0530

    fea: movie cards

--- services/api.ts
+++ services/api.ts
@@ -7,7 +7,7 @@ export const TMDB_CONFIG = {
   },
 };
 
-export const fetchMovies = async ({ query }: { query: string }) => {
+export const fetchMovies = async ({ query }: { query?: string }) => {
   const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
@@ -25,4 +25,3 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   const data = await response.json();
   return data.results;
 };
-
